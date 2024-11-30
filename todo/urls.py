654b479c603f7ad4155c006986af650c1c81677e
Mohammed <mb2019ie@gmail.com>
2024-11-30 12:58:13 +0000

    Add basic HTML

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todo/', views.todo_list, name='todo'),
+    path('todo_list/', views.todo_list, name='todo_list'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . models import Todo
 # Create your views here.
 
 def todo_list(request):
